2bc661d949fb79f9d5c28863f499a2eb8a8d1354
IghCreations <helpdesk@primitusconsultancy.com>
2026-02-28 18:54:18 +0000

    Update blog-breadcrumb-navigation.js

--- blog-breadcrumb-navigation.js
+++ blog-breadcrumb-navigation.js
@@ -1,36 +1,3 @@
 <!--- Blog breadcrumbs Squarespace 7.1 ---------------------------------->
 <!--- Copyright Primitus Consultancy [https://primitusconsultancy.com] -->
-<script>
-window.addEventListener('DOMContentLoaded', (event) => {
-
-// Variables
-let pcHome, pcBlogHome, pcBlogHomeLink, pcBlogTitle, pcBlogLink
-let pcBlogItemTitle = document.querySelector(".blog-item-title"); // Blog Title
-let link = window.location.href + '?format=json-pretty';
-let pcStringText = "";
-
-
-fetch(link)
-.then(response => {
-    // console.log('success', response);
-    return response.json();
-})
-.then(data => {
-    // console.log(data.website);
-    pcHome = data.website.authenticUrl;
-    pcBlogHome = data.collection.title;
-    pcBlogHomeLink = data.collection.fullUrl;
-    pcBlogTitle = data.item.title;
-    pcBlogLink = data.item.fullUrl;
-
-
-// Link construct
-  pcStringText = "<div class='blog-nav-display'><div class='blogcrumb'> <a  href='" + pcHome + "' >Home</a><a  href='" + pcBlogHomeLink + "' > " + pcBlogHome + "</a><a  href='#' > " + pcBlogTitle + "</a></div></div>";
-
-// Render to browser
-pcBlogItemTitle.insertAdjacentHTML("afterend", pcStringText);
-
-});
-  
-});
-</script>
+window.addEventListener("DOMContentLoaded",(e=>{let t,l,i,n,o,a=document.querySelector(".blog-item-title"),r=window.location.href+"?format=json-pretty",d="";fetch(r).then((e=>e.json())).then((e=>{t=e.website.authenticUrl,l=e.collection.title,i=e.collection.fullUrl,n=e.item.title,o=e.item.fullUrl,d="<div class='blog-nav-display'><div class='blogcrumb'> <a  href='"+t+"' >Home</a><a  href='"+i+"' > "+l+"</a><a  href='#' > "+n+"</a></div></div>",a.insertAdjacentHTML("afterend",d)}))}));
